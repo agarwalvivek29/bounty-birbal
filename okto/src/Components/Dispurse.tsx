9d03055607d18a6f32d5c325ba2557e880430dde
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-12-08 08:52:52 +0530

    update check

--- okto/src/Components/Dispurse.tsx
+++ okto/src/Components/Dispurse.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
